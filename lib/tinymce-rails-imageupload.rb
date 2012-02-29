d8d2937d3e864e03d75f20e6b7f27beb4fdf1aa5
Per Christian B. Viken <perchr@northblue.org>
2012-02-29 14:49:31 +0100

    Bundler gem skeleton

---
+++ lib/tinymce-rails-imageupload.rb
@@ -0,0 +1,9 @@
+require "tinymce-rails-imageupload/version"
+
+module Tinymce
+  module Rails
+    module Imageupload
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/tinymce-rails-imageupload/version.rb
@@ -0,0 +1,7 @@
+module Tinymce
+  module Rails
+    module Imageupload
+      VERSION = "0.0.1"
+    end
+  end
+end
