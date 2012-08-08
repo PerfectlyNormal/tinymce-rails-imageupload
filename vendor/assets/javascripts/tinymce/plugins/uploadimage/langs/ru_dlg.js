673e44205dcf878b082060ebcb8dfececc45c20e
Roman Kolesnev <kolesnev@100ege.ru>
2012-08-08 15:09:33 +0400

    russian translation

---
+++ vendor/assets/javascripts/tinymce/plugins/uploadimage/langs/ru_dlg.js
@@ -0,0 +1,7 @@
+tinyMCE.addI18n('ru.uploadimage_dlg', {
+  title: 'Вставить изображение',
+  header: "Вставить изображение",
+  input:  "Выберите изображение",
+  insert: "Вставить",
+  cancel: "Отмена"
+});
