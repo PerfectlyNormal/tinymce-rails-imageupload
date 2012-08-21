d56c5657a2a82520ba3df43908c82f6b4e4a7c5c
Per Christian B. Viken <perchr@northblue.org>
2012-08-21 10:42:09 +0200

    Add all the languages to be precompiled as well. #15

--- lib/tinymce-rails-imageupload/rails.rb
+++ lib/tinymce-rails-imageupload/rails.rb
@@ -3,7 +3,15 @@ module Tinymce
     module Imageupload
       class Engine < ::Rails::Engine
         initializer 'TinymceRailsImageupload.assets_pipeline' do |app|
-          app.config.assets.precompile << "tinymce/plugins/uploadimage/editor_plugin.js"     
+          app.config.assets.precompile << "tinymce/plugins/uploadimage/editor_plugin.js"
+          app.config.assets.precompile << "tinymce/plugins/uploadimage/langs/en.js"
+          app.config.assets.precompile << "tinymce/plugins/uploadimage/langs/en_dlg.js"
+          app.config.assets.precompile << "tinymce/plugins/uploadimage/langs/nb.js"
+          app.config.assets.precompile << "tinymce/plugins/uploadimage/langs/nb_dlg.js"
+          app.config.assets.precompile << "tinymce/plugins/uploadimage/langs/pt.js"
+          app.config.assets.precompile << "tinymce/plugins/uploadimage/langs/pt_dlg.js"
+          app.config.assets.precompile << "tinymce/plugins/uploadimage/langs/ru.js"
+          app.config.assets.precompile << "tinymce/plugins/uploadimage/langs/ru_dlg.js"
         end
       end
     end
