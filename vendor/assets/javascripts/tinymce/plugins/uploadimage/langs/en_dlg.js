ded74790ba2c7364f767a1a143775e8ab5dcce48
Per Christian B. Viken <perchr@northblue.org>
2013-01-25 16:20:08 +0100

    Refuse to submit, and display an error if no file is chosen. Fixes #1

--- vendor/assets/javascripts/tinymce/plugins/uploadimage/langs/en_dlg.js
+++ vendor/assets/javascripts/tinymce/plugins/uploadimage/langs/en_dlg.js
@@ -2,6 +2,7 @@ tinyMCE.addI18n('en.uploadimage_dlg', {
   title: 'Insert image',
   header: "Insert image",
   input:  "Choose an image",
+  blank_input: "Must choose a file",
   insert: "Insert",
   cancel: "Cancel"
 });
--- vendor/assets/javascripts/tinymce/plugins/uploadimage/langs/nb_dlg.js
+++ vendor/assets/javascripts/tinymce/plugins/uploadimage/langs/nb_dlg.js
@@ -2,6 +2,7 @@ tinyMCE.addI18n('nb.uploadimage_dlg', {
   title: 'Sett inn bilde',
   header: "Sett inn bilde",
   input:  "Velg et bilde",
+  blank_input: "M\u00e5 velge en fil",
   insert: "Sett inn",
   cancel: "Avbryt"
 });
