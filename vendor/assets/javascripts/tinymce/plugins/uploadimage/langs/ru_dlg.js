6b261309c999d207b4889084af48ddaf5c00c668
Roman Kolesnev <rvkolesnev@gmail.com>
2013-01-27 12:03:45 +0100

    Translate the new features to russian
    Signed-off-by: Per Christian B. Viken <perchr@northblue.org>

--- vendor/assets/javascripts/tinymce/plugins/uploadimage/langs/ru_dlg.js
+++ vendor/assets/javascripts/tinymce/plugins/uploadimage/langs/ru_dlg.js
@@ -2,6 +2,10 @@ tinyMCE.addI18n('ru.uploadimage_dlg', {
   title: 'Вставить изображение',
   header: "Вставить изображение",
   input:  "Выберите изображение",
+  uploading = "Загрузка…",
+  blank_input = "Необходимо выбрать файл",
+  bad_response = "Получен некорректный ответ с сервера",
+  blank_response = "Не получен ответ с сервера",
   insert: "Вставить",
   cancel: "Отмена"
 });
