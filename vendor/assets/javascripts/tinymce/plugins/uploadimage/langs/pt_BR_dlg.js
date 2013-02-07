916fe5f63d662e5a5cb5663e5b54c1e8d086857d
klebervirgilio <klebervirgilio@gmail.com>
2013-02-07 10:40:13 -0200

    Add ptBR lang pack

---
+++ vendor/assets/javascripts/tinymce/plugins/uploadimage/langs/pt_BR_dlg.js
@@ -0,0 +1,11 @@
+tinyMCE.addI18n('pt_BR.uploadimage_dlg', {
+  title: 'Inserir imagem',
+  header: "Inserir imagem",
+  input:  "Escolher uma imagem",
+  uploading: "Enviando...",
+  blank_input: "É necessário selecionar um diretório",
+  bad_response: "Resposta inesperada do servidor",
+  blank_response: "Servidor não está respondendo",
+  insert: "Inserir",
+  cancel: "Cancelar"
+});
