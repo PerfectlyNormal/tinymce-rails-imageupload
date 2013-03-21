aa13d86d3c711560cff5a2c8e885052397fc09f4
Per Christian B. Viken <perchr@northblue.org>
2013-03-21 14:33:56 +0100

    Stop listing every single file, and use some globbing

--- lib/tinymce-rails-imageupload/rails.rb
+++ lib/tinymce-rails-imageupload/rails.rb
@@ -3,17 +3,7 @@ module Tinymce
     module Imageupload
       class Engine < ::Rails::Engine
         initializer 'TinymceRailsImageupload.assets_pipeline' do |app|
-          app.config.assets.precompile << "tinymce/plugins/uploadimage/editor_plugin.js"
-          app.config.assets.precompile << "tinymce/plugins/uploadimage/langs/en.js"
-          app.config.assets.precompile << "tinymce/plugins/uploadimage/langs/en_dlg.js"
-          app.config.assets.precompile << "tinymce/plugins/uploadimage/langs/fr.js"
-          app.config.assets.precompile << "tinymce/plugins/uploadimage/langs/fr_dlg.js"
-          app.config.assets.precompile << "tinymce/plugins/uploadimage/langs/nb.js"
-          app.config.assets.precompile << "tinymce/plugins/uploadimage/langs/nb_dlg.js"
-          app.config.assets.precompile << "tinymce/plugins/uploadimage/langs/pt.js"
-          app.config.assets.precompile << "tinymce/plugins/uploadimage/langs/pt_dlg.js"
-          app.config.assets.precompile << "tinymce/plugins/uploadimage/langs/ru.js"
-          app.config.assets.precompile << "tinymce/plugins/uploadimage/langs/ru_dlg.js"
+          app.config.assets.precompile << "tinymce/plugins/uploadimage/*"
         end
       end
     end
