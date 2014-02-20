77855d0daaab4da8f816056b9bed8ffad5250cd0
Per Christian B. Viken <perchr@northblue.org>
2014-02-20 16:33:58 +0100

    TinyMCE hates <form>, loves <div>. Submitting works

--- app/assets/javascripts/tinymce/plugins/uploadimage/plugin.js
+++ app/assets/javascripts/tinymce/plugins/uploadimage/plugin.js
@@ -33,25 +33,61 @@
         }, {
           plugin_url: url
         });
+
+        /* WHY DO YOU HATE <form>, TINYMCE!? */
+        iframe = win.find("iframe")[0];
+        form = createElement('form', {
+          action: ed.getParam("uploadimage_form_url", "/tinymce_assets"),
+          target: iframe._id,
+          method: "POST",
+          enctype: 'multipart/form-data',
+          accept_charset: "UTF-8",
+        });
+        form.appendChild(createElement('input', {type: "hidden", name: "utf8", value: "✓"}));
+        form.appendChild(createElement('input', {type: 'hidden', name: 'authenticity_token', value: getMetaContents('csrf-token')}));
+        form.appendChild(createElement('input', {type: 'hidden', name: 'hint', value: ed.getParam("uploadimage_hint", "")}));
+        win.find("*").each(function(ctrl) {
+          var name = ctrl.name(), value = ctrl.value();
+          if (name && typeof(value) != "undefined") {
+            console.log("Stealing ", ctrl.getEl());
+            form.appendChild(ctrl.getEl());
+          }
+        });
+
+        var el = win.getEl();
+        var body = document.getElementById(el.id + "-body");
+        body.appendChild(form);
       }
-      
+
       function insertImage() {
-        var iframe = win.find("iframe")[0];
-        var form = createForm(iframe._id);
         // FIXME: Throbber not working
         throbber = new top.tinymce.ui.Throbber(win.find(".uploadimage_progress")[0].getEl());
         throbber.show();
 
         form.submit();
       }
-      
-      function createForm(target) {
-        var form = document.createElement('form');
-        form.action = ed.getParam("uploadimage_form_url", "/tinymce_assets");
-        form.target = target;
-        form.method = "POST";
-        return form;
+
+      function createElement(element, attributes) {
+        var el = document.createElement(element);
+        for(var property in attributes) {
+          if (!(attributes[property] instanceof Function)) {
+            el[property] = attributes[property];
+          }
+        }
+
+        return el;
       }
+
+      function getMetaContents(mn) {
+        var m = document.getElementsByTagName('meta');
+
+        for(var i in m)
+          if(m[i].name == mn)
+            return m[i].content;
+
+        return null;
+      }
+
       // Add a button that opens a window
       editor.addButton('uploadimage', {
         tooltip: ed.translate('Insert an image from your computer'),
