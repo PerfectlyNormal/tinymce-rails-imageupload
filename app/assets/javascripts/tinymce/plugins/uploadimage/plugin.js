47edb168cc08379cf4bbf56b2a6980e5973f577e
Per Christian B. Viken <perchr@northblue.org>
2014-02-20 13:10:16 +0100

    Wider dialog

--- app/assets/javascripts/tinymce/plugins/uploadimage/plugin.js
+++ app/assets/javascripts/tinymce/plugins/uploadimage/plugin.js
@@ -6,7 +6,7 @@
       var form, iframe, win, editor = ed;
       function showDialog() {
         this.win = editor.windowManager.open({
-          width:  350 + parseInt(editor.getLang('uploadimage.delta_width', 0), 10),
+          width:  400 + parseInt(editor.getLang('uploadimage.delta_width', 0), 10),
           height: 180 + parseInt(editor.getLang('uploadimage.delta_height', 0), 10),
           url: url + '/dialog.html',
         }, {
