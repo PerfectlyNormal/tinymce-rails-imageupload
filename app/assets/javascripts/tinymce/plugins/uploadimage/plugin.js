79822e2d7c1ef5cadfe8a156dde329273baa6833
Per Christian B. Viken <perchr@northblue.org>
2014-02-20 15:22:01 +0100

    Add the target iframe, and start constructing a form to submit

--- app/assets/javascripts/tinymce/plugins/uploadimage/plugin.js
+++ app/assets/javascripts/tinymce/plugins/uploadimage/plugin.js
@@ -5,21 +5,23 @@
     UploadImage: function(ed, url) {
       var form,
           iframe,
+          win,
           editor = ed;
       
       function showDialog() {
-        var win = editor.windowManager.open({
+        win = editor.windowManager.open({
           title: ed.translate('Insert an image from your computer'),
           width:  500 + parseInt(editor.getLang('uploadimage.delta_width', 0), 10),
           height: 180 + parseInt(editor.getLang('uploadimage.delta_height', 0), 10),
           body: [
+            {type: 'iframe',  id: 'hidden_upload', classes: 'uploadimage-iframe', name: 'hidden_upload', url: 'javascript:void(0)'},
             {type: 'textbox', name: 'file', label: ed.translate('Choose an image'), subtype: 'file'},
             {type: 'textbox', name: 'alt',  label: ed.translate('Image description')},
           ],
           buttons: [
             {
               text: ed.translate('Insert'),
-              onclick: function() { console.log("FIXME: Submit the form please.") }
+              onclick: insertImage,
             },
             {
               text: ed.translate('Cancel'),
@@ -29,6 +31,20 @@
         }, {
           plugin_url: url
         });
+      }
+      
+      function insertImage() {
+        var iframe = win.find("iframe")[0];
+        var form = createForm(iframe._id);
+        form.submit();
+      }
+      
+      function createForm(target) {
+        var form = document.createElement('form');
+        form.action = ed.getParam("uploadimage_form_url", "/tinymce_assets");
+        form.target = target;
+        form.method = "POST";
+        return form;
       }
       // Add a button that opens a window
       editor.addButton('uploadimage', {
