ae861242cc8c8b8a9ddf4b8549e37552a756e470
Daniel <znowm4n@gmail.com>
2017-03-06 14:52:29 +0100

    Created danish language file

---
+++ app/assets/javascripts/tinymce/plugins/uploadimage/langs/da.js
@@ -0,0 +1,14 @@
+tinyMCE.addI18n('da', {
+  'Insert an image from your computer': 'Inds\u00e6t billede fra computer',
+  'Insert image': "Inds\u00e6t billede",
+  'Choose an image': "V\u00e6lg billede",
+  'You must choose a file': "Du skal v\u00e6lge en fil",
+  'Got a bad response from the server': "Modtog et ugyldigt svar fra serveren",
+  "Didn't get a response from the server": "Modtog ikke et svar fra serveren",
+  'Insert': "Indsæt",
+  'Cancel': "Annuller",
+  'Image description': "Alternativ billedtekst",
+  'Insert an video from your computer': 'Inds\u00e6t video/lyd fra computer',
+  'Insert video': "Ind\u00e6t video/lyd",
+  'Choose an image': "V\u00e6lg video/lyd"
+});
