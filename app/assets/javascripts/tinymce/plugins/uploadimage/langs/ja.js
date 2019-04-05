af7fe80d0d5430c5718520848d7043d9c7765a78
Shota Sawada <sawada0903@gmail.com>
2019-04-05 14:21:56 +0900

    add japanese locale

---
+++ app/assets/javascripts/tinymce/plugins/uploadimage/langs/ja.js
@@ -0,0 +1,11 @@
+tinyMCE.addI18n('ja', {
+    'Insert an image from your computer': '画像を選択してください',
+    'Insert image': '画像挿入',
+    'Choose an image': "画像選択",
+    'You must choose a file': "画像を選択する必要があります",
+    'Got a bad response from the server': "サーバーエラー",
+    "Didn't get a response from the server": "サーバー応答なし",
+    'Insert': "選択",
+    'Cancel': "キャンセル",
+    'Image description': "説明文"
+});
