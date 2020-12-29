c74099b34a0b864acf84a8654fd85078137072aa
weverwijk <xS2bitbucket>
2020-12-29 23:06:18 +0100

    nl

---
+++ app/assets/javascripts/tinymce/plugins/uploadimage/langs/nl.js
@@ -0,0 +1,11 @@
+tinyMCE.addI18n('nl', {
+  'Insert an image from your computer': 'Afbeelding invoegen van je computer',
+  'Insert image': 'Afbeelding invoegen',
+  'Choose an image': "Kies een plaatje",
+  'You must choose a file': "Je moet een bestand kieen",
+  'Got a bad response from the server': "Verkeerde reactie van de server",
+  "Didn't get a response from the server": "Geen reactie van de server",
+  'Insert': "Invoegen",
+  'Cancel': "Annuleren",
+  'Image description': "Beschrijving van afbeelding",
+});
